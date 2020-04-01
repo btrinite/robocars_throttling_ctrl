07281f2fb732ef9a17d013d7a44a130f964c0814
Benoit TRINITE <btrinite@free.fr>
2020-04-01 21:53:59 +0200

    fix

--- src/robocars_throttling_ctrl.cpp
+++ src/robocars_throttling_ctrl.cpp
@@ -196,7 +196,7 @@ uint32_t mapRange(uint32_t in1,uint32_t in2,uint32_t out1,uint32_t out2,uint32_t
   return out1 + ((value-in1)*(out2-out1))/(in2-in1);
 }
 
-uint32_t mapRange(_Float32 in1,_Float32 in2,_Float32 out1,_Float32 out2,_Float32 value)
+_Float32 mapRange(_Float32 in1,_Float32 in2,_Float32 out1,_Float32 out2,_Float32 value)
 {
   if (value<in1) {value=in1;}
   if (value>in2) {value=in2;}
